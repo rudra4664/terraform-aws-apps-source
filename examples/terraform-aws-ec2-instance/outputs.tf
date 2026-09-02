e4a4077c93d7d5606abd16636f9c050d5bc85396
rudra4664 <167764527+rudra4664@users.noreply.github.com>
2026-09-02 23:06:06 +0530

    Define outputs for EC2 instance details
    Add outputs for instance ID, private IP, and additional volume IDs.

--- examples/terraform-aws-ec2-instance/outputs.tf
+++ examples/terraform-aws-ec2-instance/outputs.tf
@@ -1,1 +1,14 @@
+output "instance_id" {
+  description = "ID of the created EC2 instance."
+  value       = module.ec2.resources.instance.id
+}
 
+output "instance_private_ip" {
+  description = "Primary private IP address of the created EC2 instance."
+  value       = module.ec2.resources.primary_network_interface.private_ip
+}
+
+output "additional_volume_ids" {
+  description = "IDs of the additional EBS volumes attached to the instance."
+  value       = { for k, v in module.ec2.resources.additional_volumes : k => v.id }
+}
