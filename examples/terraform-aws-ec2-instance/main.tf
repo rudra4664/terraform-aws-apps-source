5bf5670a0300184662e673c202826cf46ff53668
rudra4664 <167764527+rudra4664@users.noreply.github.com>
2026-09-02 23:05:31 +0530

    Add EC2 module configuration in main.tf

--- examples/terraform-aws-ec2-instance/main.tf
+++ examples/terraform-aws-ec2-instance/main.tf
@@ -1,1 +1,10 @@
-
+module "ec2" {
+  source                        = "app.terraform.io/NYL-Prod/apps-source/aws//modules/terraform-aws-ec2-instance"
+  lob                           = var.lob
+  env                           = var.env
+  instance                      = var.ec2.instance
+  primary_network_interface     = var.ec2.primary_network_interface
+  additional_volumes            = var.ec2.additional_volumes
+  additional_network_interfaces = var.ec2.additional_network_interfaces
+  tags                          = var.ec2.tags
+}
