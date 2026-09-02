00979518c49ad93d788bc19851f465d1ed989d0c
rudra4664 <167764527+rudra4664@users.noreply.github.com>
2026-09-02 23:06:35 +0530

    Add Terraform configuration for AWS provider
    Configure Terraform to require AWS provider version 6.45 or higher and set up provider settings.

--- examples/terraform-aws-ec2-instance/providers.tf
+++ examples/terraform-aws-ec2-instance/providers.tf
@@ -1,1 +1,26 @@
+terraform {
+  required_version = ">= 1.15"
 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.45"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+
+  ignore_tags {
+    keys = [
+      "domain_join",
+      "fqdn",
+      "nyl:appid",
+    ]
+
+    key_prefixes = [
+      "nyl:platform:",
+    ]
+  }
+}
